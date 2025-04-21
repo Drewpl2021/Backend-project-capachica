cc8a7f42d0277a29a46b40942c2a7932acdb5b07
Alessandro Pastor <alexkip159alex@gmail.com>
2025-04-21 02:03:47 -0500

    CAMBIOS

--- database/migrations/2025_04_21_034941_create_asociacions_table.php
+++ database/migrations/2025_04_21_034941_create_asociacions_table.php
@@ -13,11 +13,15 @@ return new class extends Migration
     {
         Schema::create('asociacions', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->uuid('municipalidad_id'); // Clave foránea que hace referencia a la tabla 'municipalidads'
             $table->string('nombres');
             $table->string('descripcion');
             $table->string('lugar');
             $table->boolean('status');
-            $table->foreignId('municipalidad_id')->constrained('municipalidads');
+            $table->foreign('municipalidad_id')
+                ->references('id')
+                ->on('municipalidads')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
