be291dd1bd57b597829a584e6cf0968d8628b56b
drewpl2021 <linoplas0@gmail.com>
2025-05-15 09:48:43 -0500

    cambios

--- database/seeders/ImgAsociacionSeeder.php
+++ database/seeders/ImgAsociacionSeeder.php
@@ -20,19 +20,19 @@ class ImgAsociacionSeeder extends Seeder
             // Crear algunas imágenes de asociación para cada asociación
             Img_Asociacion::create([
                 'asociacion_id' => $asociacion->id,
-                'url_image' => 'https://example.com/imagen1.jpg',
+                'url_image' => 'https://consultasenlinea.mincetur.gob.pe/fichaInventario/foto.aspx?cod=471157',
                 'estado' => true,
                 'codigo' => 101
             ]);
             Img_Asociacion::create([
                 'asociacion_id' => $asociacion->id,
-                'url_image' => 'https://example.com/imagen2.jpg',
+                'url_image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYpsgH3y4Sc4KtA_O9XOL9M2f1kHvPCXM9Dg&s',
                 'estado' => true,
                 'codigo' => 102
             ]);
             Img_Asociacion::create([
                 'asociacion_id' => $asociacion->id,
-                'url_image' => 'https://example.com/imagen3.jpg',
+                'url_image' => 'https://losviajesdeali.com/wp-content/uploads/2015/01/pen%C3%ADnsula-de-capachica-1-18.jpg',
                 'estado' => true,
                 'codigo' => 103
             ]);
