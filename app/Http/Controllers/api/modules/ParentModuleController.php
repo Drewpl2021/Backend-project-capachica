9eb8b63aa1d6ac563aa58e1e8b23ae62162838a3
Alessandro Pastor <alexkip159alex@gmail.com>
2025-05-29 14:16:52 -0500

    s

--- app/Http/Controllers/api/modules/ParentModuleController.php
+++ app/Http/Controllers/api/modules/ParentModuleController.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\ParentModule;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
 
 class ParentModuleController extends Controller
 {
